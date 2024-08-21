56c7a6627e243961b270b1099f3f097f85ce62e2
Finley <finley.mwachia12@gmail.com>
2024-08-21 09:13:03 +0300

    improved efficiency of map function by shortening it

--- src/index.jsx
+++ src/index.jsx
@@ -6,30 +6,23 @@ ReactDOM.render(<App />, document.getElementById("root"));
 
 // var numbers = [3, 56, 2, 48, 5];
 
-////Map -Create a new array by doing something with each item in an array.
-// const newNumbers = numbers.map(function (x) {
-//   return x * 2;
-// });
+//Map -Create a new array by doing something with each item in an array.
+// const newNumbers = numbers.map( x => x * 2);
 
-//////Filter - Create a new array by keeping the items that return true.
-// const newNumbers = numbers.filter(function(num) {
-//   return num < 10;
-// });
 
-//Reduce - Accumulate a value by doing something to each item in an array.
-// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
-//     return accumulator + currentNumber;
-// })
+// ////Filter - Create a new array by keeping the items that return true.
+// const newNumbers = numbers.filter(num => num < 10);
 
-////Find - find the first item that matches from an array.
-// const newNumber = numbers.find(function (num) {
-//   return num > 10;
-// })
+// Reduce - Accumulate a value by doing something to each item in an array.
+// var newNumber = numbers.reduce( (accumulator, currentNumber) => accumulator + currentNumber);
 
-////FindIndex - find the index of the first item that matches.
-// const newNumber = numbers.findIndex(function (num) {
-//   return num > 10;
-// })
+// //Find - find the first item that matches from an array.
+// const newNumber = numbers.find( num =>num > 10);
+
+// //FindIndex - find the index of the first item that matches.
+// const newNumber = numbers.findIndex( (num) =>  num > 10)
+
+
 
 // If you're running this locally in VS Code use the commands:
 // npm install
